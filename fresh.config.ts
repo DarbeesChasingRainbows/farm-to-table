864684a36cc235b1d779d0204cab817e8f59e736
DarbeesChasingRainbows <148270288+DarbeesChasingRainbows@users.noreply.github.com>
2025-03-29 12:12:19 -0500

    Add Fresh framework configuration

---
+++ fresh.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "$fresh/server.ts";
+import tailwind from "$fresh/plugins/tailwind.ts";
+import twindConfig from "./twind.config.ts";
+
+export default defineConfig({
+  plugins: [
+    tailwind({
+      config: twindConfig,
+    }),
+  ],
+});
